b61cbd7aa5f7a4840ad3a42713a793b7e1c18d69
Rees Mortensen <reesmortensen@Reess-MacBook-Air-2.local>
2023-09-22 08:06:25 -0400

    app.js commit

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err})
 })
 
-// const port = process.env.PORT || 3000;//cant get to work
-app.listen(3000, () => {
-    console.log("Serving on port 3000")
+ const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
